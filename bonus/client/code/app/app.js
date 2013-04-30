e632e34ae06b7d227256ca51ce448fee36f44d69
Johan Euphrosine <proppy@google.com>
2013-04-30 22:25:42 +0000

    add bonus todo steps

--- bonus/client/code/app/app.js
+++ bonus/client/code/app/app.js
@@ -1,206 +1,206 @@
 /*global $, ss*/
 (function () {
-	'use strict';
+  'use strict';
 
-	var Utils = {
-		// https://gist.github.com/1308368
-		uuid: function(a,b){for(b=a='';a++<36;b+=a*51&52?(a^15?8^Math.random()*(a^20?16:4):4).toString(16):'-');return b},
-		pluralize: function (count, word) {
-			return count === 1 ? word : word + 's';
-		}
-	};
+  var Utils = {
+    // https://gist.github.com/1308368
+    uuid: function(a,b){for(b=a='';a++<36;b+=a*51&52?(a^15?8^Math.random()*(a^20?16:4):4).toString(16):'-');return b},
+    pluralize: function (count, word) {
+      return count === 1 ? word : word + 's';
+    }
+  };
 
-	var App = {
-		init: function () {
-			var self = this;
-			this.ENTER_KEY = 13;
+  var App = {
+    init: function () {
+      var self = this;
+      this.ENTER_KEY = 13;
 
-			ss.rpc('todos.getAll', function (todos) {
-				self.todos = todos;
-				self.cacheElements();
-				self.bindEvents();
-				self.render();
-			});
-		},
+      ss.rpc('todo4.getAll', function (todos) {
+	self.todos = todos;
+	self.cacheElements();
+	self.bindEvents();
+	self.render();
+      });
+    },
 
-		cacheElements: function () {
-			this.$todoApp = $('#todoapp');
-			this.$newTodo = $('#new-todo');
-			this.$toggleAll = $('#toggle-all');
-			this.$main = $('#main');
-			this.$todoList = $('#todo-list');
-			this.$footer = this.$todoApp.find('#footer');
-			this.$count = $('#todo-count');
-			this.$clearBtn = $('#clear-completed');
-		},
+    cacheElements: function () {
+      this.$todoApp = $('#todoapp');
+      this.$newTodo = $('#new-todo');
+      this.$toggleAll = $('#toggle-all');
+      this.$main = $('#main');
+      this.$todoList = $('#todo-list');
+      this.$footer = this.$todoApp.find('#footer');
+      this.$count = $('#todo-count');
+      this.$clearBtn = $('#clear-completed');
+    },
 
-		bindEvents: function () {
-			var list = this.$todoList;
+    bindEvents: function () {
+      var list = this.$todoList;
 
-			this.$newTodo.on('keyup', this.create);
-			this.$toggleAll.on('change', this.toggleAll);
-			this.$footer.on('click', '#clear-completed', this.destroyCompleted);
+      this.$newTodo.on('keyup', this.create);
+      this.$toggleAll.on('change', this.toggleAll);
+      this.$footer.on('click', '#clear-completed', this.destroyCompleted);
 
-			list.on('change', '.toggle', this.toggle);
-			list.on('dblclick', 'label', this.edit);
-			list.on('keypress', '.edit', this.blurOnEnter);
-			list.on('blur', '.edit', this.update);
-			list.on('click', '.destroy', this.destroy);
+      list.on('change', '.toggle', this.toggle);
+      list.on('dblclick', 'label', this.edit);
+      list.on('keypress', '.edit', this.blurOnEnter);
+      list.on('blur', '.edit', this.update);
+      list.on('click', '.destroy', this.destroy);
 
-			ss.event.on('updateTodos', this.updateTodos);
-			ss.event.on('updateTodo', this.updateTodo);
-			ss.event.on('removeTodo', this.removeTodo);
-		},
+      ss.event.on('updateTodos', this.updateTodos);
+      ss.event.on('updateTodo', this.updateTodo);
+      ss.event.on('removeTodo', this.removeTodo);
+    },
 
-		updateTodos: function (todos) {
-			App.todos = todos;
+    updateTodos: function (todos) {
+      App.todos = todos;
 
-			App.render(true);
-		},
+      App.render(true);
+    },
 
-        updateTodo: function(todo) {
-            App.todos[todo.id] = todo;
-		    App.render(true);
-        },
+    updateTodo: function(todo) {
+      App.todos[todo.id] = todo;
+      App.render(true);
+    },
 
-        removeTodo: function(todoId) {
-            delete App.todos[todoId];
-		    App.render(true);
-        },
+    removeTodo: function(todoId) {
+      delete App.todos[todoId];
+      App.render(true);
+    },
 
-		render: function (preventRpc) {
-		    var html = $.map(this.todos, function (el) {
-				return ss.tmpl.todo.render(el);
-			}).join('');
+    render: function (preventRpc) {
+      var html = $.map(this.todos, function (el) {
+	return ss.tmpl.todo.render(el);
+      }).join('');
 
-			this.$todoList.html(html);
-		    this.$main.toggle(!!Object.keys(this.todos).length);
-			this.$toggleAll.prop('checked', !this.activeTodoCount());
+      this.$todoList.html(html);
+      this.$main.toggle(!!Object.keys(this.todos).length);
+      this.$toggleAll.prop('checked', !this.activeTodoCount());
 
-			this.renderFooter();
-		},
+      this.renderFooter();
+    },
 
-		renderFooter: function () {
-		    var todoCount = Object.keys(this.todos).length;
+    renderFooter: function () {
+      var todoCount = Object.keys(this.todos).length;
 
-			var activeTodoCount = this.activeTodoCount();
+      var activeTodoCount = this.activeTodoCount();
 
-			var footer = {
-				activeTodoCount: activeTodoCount,
-				activeTodoWord: Utils.pluralize(activeTodoCount, 'item'),
-				completedTodos: todoCount - activeTodoCount
-			};
+      var footer = {
+	activeTodoCount: activeTodoCount,
+	activeTodoWord: Utils.pluralize(activeTodoCount, 'item'),
+	completedTodos: todoCount - activeTodoCount
+      };
 
-			this.$footer.toggle(!!todoCount);
+      this.$footer.toggle(!!todoCount);
 
-			this.$footer.html( ss.tmpl.footer.render(footer) );
-		},
+      this.$footer.html( ss.tmpl.footer.render(footer) );
+    },
 
-		toggleAll: function () {
-			var isChecked = $(this).prop('checked');
+    toggleAll: function () {
+      var isChecked = $(this).prop('checked');
 
-			$.each(App.todos, function (i, val) {
-				val.completed = isChecked;
-                ss.rpc('todos.update', val);
-			});
+      $.each(App.todos, function (i, val) {
+	val.completed = isChecked;
+        ss.rpc('todo4.update', val);
+      });
 
-			App.render();
-		},
+      App.render();
+    },
 
-		activeTodoCount: function () {
-			var count = 0;
+    activeTodoCount: function () {
+      var count = 0;
 
-			$.each(this.todos, function (i, val) {
-				if (!val.completed) {
-					count++;
-				}
-			});
+      $.each(this.todos, function (i, val) {
+	if (!val.completed) {
+	  count++;
+	}
+      });
 
-			return count;
-		},
+      return count;
+    },
 
-		destroyCompleted: function () {
-			var todos = App.todos;
-			var l = todos.length;
-			while (l--) {
-				if (todos[l].completed) {
-					todos.splice(l, 1);
-				}
-			}
-            ss.rpc('todos.archive');
-			App.render();
-		},
+    destroyCompleted: function () {
+      var todos = App.todos;
+      var l = todos.length;
+      while (l--) {
+	if (todos[l].completed) {
+	  todos.splice(l, 1);
+	}
+      }
+      ss.rpc('todo4.archive');
+      App.render();
+    },
 
-		// Accepts an element from inside the ".item" div and
-		// returns the corresponding todo in the todos array
-		getTodo: function (elem, callback) {
-			var id = $(elem).closest('li').data('id');
-            callback.call(App, id, this.todos[id]);
-		},
+    // Accepts an element from inside the ".item" div and
+    // returns the corresponding todo in the todos array
+    getTodo: function (elem, callback) {
+      var id = $(elem).closest('li').data('id');
+      callback.call(App, id, this.todos[id]);
+    },
 
-		create: function (e) {
-			var $input = $(this);
-			var val = $.trim($input.val());
+    create: function (e) {
+      var $input = $(this);
+      var val = $.trim($input.val());
 
-			if (e.which !== App.ENTER_KEY || !val) {
-				return;
-			}
+      if (e.which !== App.ENTER_KEY || !val) {
+	return;
+      }
 
-            var id = Utils.uuid();
-            var todo = {
-				id: id,
-				title: val,
-				completed: false
-			};
-		    App.todos[id] = todo;
-            ss.rpc('todos.update', todo);
+      var id = Utils.uuid();
+      var todo = {
+	id: id,
+	title: val,
+	completed: false
+      };
+      App.todos[id] = todo;
+      ss.rpc('todo4.update', todo);
 
-			$input.val('');
+      $input.val('');
 
-			App.render();
-		},
+      App.render();
+    },
 
-		toggle: function () {
-			App.getTodo(this, function (i, val) {
-				val.completed = !val.completed;
-                ss.rpc('todos.update', val);
-			});
+    toggle: function () {
+      App.getTodo(this, function (i, val) {
+	val.completed = !val.completed;
+        ss.rpc('todo4.update', val);
+      });
 
-			App.render();
-		},
+      App.render();
+    },
 
-		edit: function () {
-			$(this).closest('li').addClass('editing').find('.edit').focus();
-		},
+    edit: function () {
+      $(this).closest('li').addClass('editing').find('.edit').focus();
+    },
 
-		blurOnEnter: function (e) {
-			if (e.keyCode === App.ENTER_KEY) {
-				e.target.blur();
-			}
-		},
+    blurOnEnter: function (e) {
+      if (e.keyCode === App.ENTER_KEY) {
+	e.target.blur();
+      }
+    },
 
-		update: function () {
-			var val = $.trim($(this).removeClass('editing').val());
-		    App.getTodo(this, function (id, todo) {
-				if (val) {
-					todo.title = val;
-                    ss.rpc('todos.update', todo);
-				} else {
-                    ss.rpc('todos.remove', id);
-				    delete this.todos[id];
-				}
-				this.render();
-			});
-		},
+    update: function () {
+      var val = $.trim($(this).removeClass('editing').val());
+      App.getTodo(this, function (id, todo) {
+	if (val) {
+	  todo.title = val;
+          ss.rpc('todo4.update', todo);
+	} else {
+          ss.rpc('todo4.remove', id);
+	  delete this.todos[id];
+	}
+	this.render();
+      });
+    },
+    
+    destroy: function () {
+      App.getTodo(this, function (id, todo) {
+        ss.rpc('todo4.remove', id);
+        delete this.todos[id];
+	this.render();
+      });
+    }
+  };
 
-		destroy: function () {
-		        App.getTodo(this, function (id, todo) {
-                ss.rpc('todos.remove', id);
-                delete this.todos[id];
-				this.render();
-			});
-		}
-	};
-
-	App.init();
+  App.init();
 })();
---
+++ bonus/server/rpc/todo4.js
@@ -0,0 +1,49 @@
+// Server-side code
+
+// All the todos are stored in an in-memory array on the server
+// This should not be done in production apps
+var googleapis = require('googleapis');
+var authclient = new googleapis.OAuth2Client();
+var datastore;
+var compute;
+var todoListName = process.argv[2] || 'default';
+var datasetId = 'gcd-codelab';
+
+googleapis.discover('datastore', 'v1beta1', {
+  localDiscoveryFilePath: './datastore_v1beta1.json',
+}).execute(function(err, client) {
+  console.log(err, client);
+  compute = new googleapis.auth.Compute()
+  compute.authorize(function(err, result) {
+    console.log(err, result);
+    datastore = client.datastore.datasets;
+  });
+});
+
+var __TODO__ = null;
+// Define actions which can be called from the client using
+// ss.rpc('demo.ACTIONNAME', param1, param2...)
+exports.actions = function(req, res, ss) {
+  return {
+    getAll: function () {
+      var todos = {};
+      datastore.__TODO__(); // copy commands.ls implementatation
+      res(tt);
+    },
+    remove: function(todoId) {
+      ss.publish.all('removeTodo', todoId);
+      datastore.__TODO__(); // copy commands.del implementatation
+    },
+    update: function(todo) {
+      ss.publish.all('updateTodo', todo);
+      datastore.__TODO__(); // copy commands.edit implementatation
+    },
+    archive: function() {
+      datastore.__TODO__().execute(function(err, result) { // copy commands.archive implementation
+        __TODO__forEach(function() { // for each deleted todos
+                ss.publish.all('removeTodo', id);
+        });
+      });
+    }
+  };
+};
