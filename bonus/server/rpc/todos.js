375d9a1fd39bae254d30cf46237e4fd44e3b6057
Johan Euphrosine <proppy@google.com>
2013-05-06 17:23:27 +0000

    s/pathElements/path/

--- bonus/server/rpc/todos.js
+++ bonus/server/rpc/todos.js
@@ -1,5 +1,7 @@
 // Server-side code
 
+// All the todos are stored in an in-memory array on the server
+// This should not be done in production apps
 var googleapis = require('googleapis');
 var authclient = new googleapis.OAuth2Client();
 var datastore;
@@ -18,156 +20,28 @@ googleapis.discover('datastore', 'v1beta1', {
   });
 });
 
+var __TODO__ = null;
 // Define actions which can be called from the client using
 // ss.rpc('demo.ACTIONNAME', param1, param2...)
 exports.actions = function(req, res, ss) {
   return {
     getAll: function () {
-      datastore.runquery({
-        datasetId: datasetId,
-        query: {
-          kinds: [{
-            name: 'Todo',
-          }],
-          filter: {
-            propertyFilter: {
-              property: {
-                name: '__key__'
-              },
-              operator: 'hasAncestor',
-              value: {
-                keyValue: {
-                  pathElements: [{
-                    kind: 'TodoList',
-                    name: todoListName
-                  }]
-                }
-              }
-            }
-          }
-        }   
-      }).withAuthClient(compute).execute(function(err, result) {
-        var tt = {};
-        var entityResults = result.batch.entityResults || [];
-        entityResults.forEach(function(entityResult) {
-          var id = entityResult.entity.key.pathElements[1].name;
-          var title = entityResult.entity.properties.title.values[0].stringValue;
-          var completed = entityResult.entity.properties.completed.values[0].booleanValue;
-          tt[id] = {
-            id: id,
-            title: title,
-            completed: completed
-          };
-        });
-        res(tt);
-      });
+      var todos = {};
+      datastore.__TODO__(); // copy commands.ls implementatation
+      res(tt);
     },
     remove: function(todoId) {
       ss.publish.all('removeTodo', todoId);
-      datastore.blindwrite({
-        datasetId: datasetId,
-        mutation: {
-          delete: [{
-            pathElements: [{
-              kind: 'TodoList',
-              name: todoListName,
-            },{
-              kind: 'Todo',
-              name: todoId
-            }]
-          }]
-        }      
-      }).withAuthClient(compute).execute(function(err, result) {
-        console.log(err, result);
-      });
+      datastore.__TODO__(); // copy commands.del implementatation
     },
     update: function(todo) {
       ss.publish.all('updateTodo', todo);
-      datastore.blindwrite({
-        datasetId: datasetId,
-        mutation: {
-          upsert: [{
-            key: {
-              pathElements: [,{
-                kind: 'TodoList',
-                name: todoListName,
-              },{
-                kind: 'Todo',
-                name: todo.id
-              }]
-            },
-            properties: {
-              title: {
-                values: [{
-                  stringValue: todo.title
-                }]
-              },
-              completed: {
-                values: [{
-                  booleanValue: todo.completed
-                }]
-              }
-            }
-          }]
-        }
-      }).withAuthClient(compute).execute(function(err, result) {
-        console.log(err, result);
-      });
+      datastore.__TODO__(); // copy commands.edit implementatation
     },
     archive: function() {
-      datastore.runquery({
-        datasetId: datasetId,
-        query: {
-          kinds: [{
-            name: 'Todo',
-          }],
-          filter: {
-            compositeFilter: {
-              operator: 'and',
-              filters: [{
-                propertyFilter: {
-                  property: {
-                    name: '__key__'
-                  },
-                  operator: 'hasAncestor',
-                  value: {
-                    keyValue: {
-                      pathElements: [{
-                        kind: 'TodoList',
-                              name: todoListName
-                      }]
-                    }
-                        }
-                }
-              },{
-                propertyFilter: {
-                  property: {
-                    name: 'completed'
-                  },
-                  operator: 'equal',
-                  value: {
-                    booleanValue: true
-                  }
-                }
-              }]
-            }
-          }
-        }   
-      }).withAuthClient(compute).execute(function(err, result) {
-        var keys = [];
-        var entityResults = result.batch.entityResults || [];
-        entityResults.forEach(function(entityResult) {
-          keys.push(entityResult.entity.key);
-          var id = entityResult.entity.key.pathElements[1].name;
-          ss.publish.all('removeTodo', id);
-        });
-        datastore.blindwrite({
-          datasetId: datasetId,
-          mutation: {
-            delete: keys
-          }      
-        }).withAuthClient(compute).execute(function(err, result) {
-          console.log(err, result);
+      datastore.__TODO__().execute(function(err, result) { // copy commands.archive implementation
+        __TODO__forEach(function() { // for each deleted todos
+                ss.publish.all('removeTodo', id);
         });
       });
     }
