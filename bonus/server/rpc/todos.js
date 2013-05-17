b4f3f11507ae490cda7e9f08189f6b777ab743dd
Johan Euphrosine <proppy@google.com>
2013-05-17 17:55:56 +0000

    restore todos.js

--- bonus/server/rpc/todos.js
+++ bonus/server/rpc/todos.js
@@ -22,137 +22,33 @@ compute.authorize(function(err, result) {
 exports.actions = function(req, res, ss) {
   return {
     getAll: function () {
-      datastore.runQuery({
-        datasetId: datasetId,
-        query: {
-          kinds: [{ name: 'Todo' }],
-          filter: {
-            propertyFilter: {
-              property: { name: '__key__' },
-              operator: 'hasAncestor',
-              value: {
-                keyValue: {
-                  path: [{ kind: 'TodoList', name: todoListName }]
-                }
-              }
-            }
-          }
-        }   
-      }).execute(function(err, result) {
-        var tt = {};
-        var entityResults = result.batch.entityResults || [];
-        entityResults.forEach(function(entityResult) {
-          var id = entityResult.entity.key.path[1].id;
-          var title = entityResult.entity.properties.title.values[0].stringValue;
-          var completed = entityResult.entity.properties.completed.values[0].booleanValue;
-          tt[id] = {
-            id: id,
-            title: title,
-            completed: completed
-          };
-        });
-        res(tt);
-      });
+      var todos = {};
+      datastore.__FIXME__(); // copy commands.ls implementatation
+      res(tt);
     },
     remove: function(id) {
       ss.publish.all('removeTodo', id);
-      datastore.blindWrite({
-        datasetId: datasetId,
-        mutation: {
-          delete: [{
-            path: [{ kind: 'TodoList', name: todoListName },
-                   { kind: 'Todo', id: id }]
-          }]
-        }      
-      }).execute(function(err, result) {
-        console.log(err, result);
-      });
+      datastore.__FIXME__(); // copy commands.del implementatation
     },
     create: function(todo) {
-      datastore.blindWrite({
-        datasetId: datasetId,
-        mutation: {
-          insertAutoId: [{
-            key: {
-              path: [{ kind: 'TodoList', name: todoListName },
-                     { kind: 'Todo'}]
-            },
-            properties: {
-              title: { values: [{ stringValue: todo.title }] },
-              completed: { values: [{ booleanValue: todo.completed }] }
-            }
-          }]
-        }
-      }).execute(function(err, result) {
+      datastore.blindWrite(
+        __FIXME__; // copy commands.add implementatation and tweak a little
+      ).execute(function(err, result) {
         console.log(err, result);
         var id = result.mutationResult.insertAutoIdKeys[0].path[1].id;
         todo.id = id;
+        // need to publish here
         ss.publish.all('updateTodo', todo);
       });
     },
     update: function(todo) {
       ss.publish.all('updateTodo', todo);
-      datastore.blindWrite({
-        datasetId: datasetId,
-        mutation: {
-          upsert: [{
-            key: {
-              path: [{ kind: 'TodoList', name: todoListName },
-                     { kind: 'Todo', id: todo.id }]
-            },
-            properties: {
-              title: { values: [{ stringValue: todo.title }] },
-              completed: { values: [{ booleanValue: todo.completed }] }
-            }
-          }]
-        }
-      }).execute(function(err, result) {
-        console.log(err, result);
-      });
+      datastore.__FIXME__(); // copy commands.edit implementatation
     },
     archive: function() {
-      datastore.runQuery({
-        datasetId: datasetId,
-        query: {
-          kinds: [{ name: 'Todo' }],
-          filter: {
-            compositeFilter: {
-              operator: 'and',
-              filters: [{
-                propertyFilter: {
-                  property: { name: '__key__' },
-                  operator: 'hasAncestor',
-                  value: {
-                    keyValue: {
-                      path: [{ kind: 'TodoList', name: todoListName }]
-                    }
-                  }
-                }
-              },{
-                propertyFilter: {
-                  property: { name: 'completed' },
-                  operator: 'equal',
-                  value: { booleanValue: true }
-                }
-              }]
-            }
-          }
-        }   
-      }).execute(function(err, result) {
-        var keys = [];
-        var entityResults = result.batch.entityResults || [];
-        entityResults.forEach(function(entityResult) {
-          keys.push(entityResult.entity.key);
-          var id = entityResult.entity.key.path[1].id;
-          ss.publish.all('removeTodo', id);
-        });
-        datastore.blindWrite({
-          datasetId: datasetId,
-          mutation: {
-            delete: keys
-          }      
-        }).execute(function(err, result) {
-          console.log(err, result);
+      datastore.__FIXME__().execute(function(err, result) { // copy commands.archive implementation
+        __FIXME__forEach(function() { // for each deleted todos
+                ss.publish.all('removeTodo', id);
         });
       });
     }
